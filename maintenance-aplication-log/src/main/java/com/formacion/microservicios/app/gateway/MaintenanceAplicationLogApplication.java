c322a20f34b435bdce132dc7382523a704704f30
jmtomeX <jmtome00@gmail.com>
2021-03-05 19:52:22 +0100

    Creado proyecto con conexión a MySql. Close #1

---
+++ maintenance-aplication-log/src/main/java/com/formacion/microservicios/app/gateway/MaintenanceAplicationLogApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.microservicios.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceAplicationLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceAplicationLogApplication.class, args);
+	}
+
+}
---
+++ maintenance-aplication-log/src/test/java/com/formacion/microservicios/app/gateway/MaintenanceAplicationLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.microservicios.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceAplicationLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
