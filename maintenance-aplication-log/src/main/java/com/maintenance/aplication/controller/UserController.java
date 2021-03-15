c3c967e1b7c44a4ff8dd1f9d28f355be699abef2
jmtomeX <jmtome00@gmail.com>
2021-03-15 19:34:42 +0100

    crear llamada para cambiar passsword

--- maintenance-aplication-log/src/main/java/com/maintenance/aplication/controller/UserController.java
+++ maintenance-aplication-log/src/main/java/com/maintenance/aplication/controller/UserController.java
@@ -1,17 +1,23 @@
 package com.maintenance.aplication.controller;
 
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.maintenance.aplication.dto.ChangePasswordForm;
 import com.maintenance.aplication.entity.User;
 import com.maintenance.aplication.respository.RoleRepository;
 import com.maintenance.aplication.service.UserService;
@@ -48,7 +54,7 @@ public class UserController {
 		} else {
 
 			try {
-				
+
 				userService.createUser(user);
 				model.addAttribute("userForm", new User());
 				model.addAttribute("listTab", "active");
@@ -68,23 +74,27 @@ public class UserController {
 
 	@GetMapping("/editUser/{id}")
 	public String getEditUserForm(Model model, @PathVariable(name = "id") Long id) throws Exception {
-		
+
 		User userToEdit = userService.getUserById(id);
 		model.addAttribute("userForm", userToEdit);
 		model.addAttribute("userList", userService.getAllUsers());
 		model.addAttribute("roles", roleRepository.findAll());
 		model.addAttribute("formTab", "active");
 		model.addAttribute("editMode", "true");
+		// cada vez que se agrefa el formulario se recoge el id del usuario
+		model.addAttribute("passwordForm", new ChangePasswordForm(id));
 
 		return "user-form/user-view";
 	}
-	
+
 	@PostMapping("/editUser")
 	public String editUser(@Valid @ModelAttribute("userForm") User user, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			model.addAttribute("userForm", user);
 			model.addAttribute("formTab", "active");
 			model.addAttribute("editMode", "true");
+			// para actualizar la contraseña
+			model.addAttribute("passwordForm", new ChangePasswordForm(user.getId()));
 
 		} else {
 
@@ -99,26 +109,45 @@ public class UserController {
 				model.addAttribute("formTab", "active");
 				model.addAttribute("userList", userService.getAllUsers());
 				model.addAttribute("roles", roleRepository.findAll());
-				// se queda en la misma pantalla si hay un error 
+				// se queda en la misma pantalla si hay un error
 				model.addAttribute("editMode", "false");
+				model.addAttribute("passwordForm", new ChangePasswordForm(user.getId()));
 			}
 		}
 		model.addAttribute("userList", userService.getAllUsers());
 		model.addAttribute("roles", roleRepository.findAll());
 		return "user-form/user-view";
 	}
+
 	@GetMapping("/userForm/cancel")
 	public String cancelEditUser(ModelMap model) {
 		return "redirect:/userForm";
 	}
+
 	@GetMapping("/deleteUser/{id}")
-	public String deleteUSer(Model model, @PathVariable(name="id") Long id ) {
+	public String deleteUSer(Model model, @PathVariable(name = "id") Long id) {
 		try {
 			userService.deleteUser(id);
-		} catch(Exception e) {
-			model.addAttribute("listErrorMessage",e.getMessage());
+		} catch (Exception e) {
+			model.addAttribute("listErrorMessage", e.getMessage());
 		}
-		
 		return "redirect:/userForm";
 	}
+
+	@PostMapping("/editUser/changePassword")
+	public ResponseEntity<String> editUserChangePassword(@Valid @RequestBody ChangePasswordForm form, Errors errors) {
+		try {
+			if (errors.hasErrors()) {
+				// si hay errores se lanzan en una excepción con un string
+				String result = errors.getAllErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.joining(""));
+				throw new Exception(result);
+			}
+			userService.changePassword(form);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		return ResponseEntity.ok("Success");
+
+	}
+
 }
