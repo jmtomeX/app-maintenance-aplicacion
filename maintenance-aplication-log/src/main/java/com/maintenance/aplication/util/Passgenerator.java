2f8ab4fc42877c0ecfec2b3a7742bf90f0887eb7
jmtomeX <jmtome00@gmail.com>
2021-03-16 20:26:00 +0100

    texto para lanzar la función de encriptación y recoger la contraseña para cambiarla en la bbdd de los usurios creados

--- maintenance-aplication-log/src/main/java/com/maintenance/aplication/util/Passgenerator.java
+++ maintenance-aplication-log/src/main/java/com/maintenance/aplication/util/Passgenerator.java
@@ -6,7 +6,7 @@ public class Passgenerator {
 	public static void main(String... args) {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(4);
 
-		// El String que mandamos al metodo encode es el password que queremos encriptar.
+		// para cambiar las contraseñas de los usuarios ya creados, clic derecho y run as -->> aplicación java y muestra en la consola
 		System.out.println(bCryptPasswordEncoder.encode("1234"));
 		/*
 		 * Resultado: $2a$04$n6WIRDQlIByVFi.5rtQwEOTAzpzLPzIIG/O6quaxRKY2LlIHG8uty
